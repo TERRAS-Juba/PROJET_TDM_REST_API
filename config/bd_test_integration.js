f8cd136e0828ac638a0a02e151485ea5b5f57bb7
TERRAS Juba <ij_terras@esi.dz>
2022-06-22 18:14:24 +0100

    Portage de l'application dans le cloud

--- config/bd_test_integration.js
+++ config/bd_test_integration.js
@@ -9,10 +9,9 @@ const Pool = require("pg").Pool;
 });
 */
 const poolTestIntegration = new Pool({
-  user: "dovsksxwglnxdf",
-  host: "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com",
-  database: "d3vj2jdd55lhaf",
-  password: "416dc753d9d1db383dec8e8ff7fdfdc14ffa7b1c8147a88860c1346cfd6adad1",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 module.exports = poolTestIntegration;
