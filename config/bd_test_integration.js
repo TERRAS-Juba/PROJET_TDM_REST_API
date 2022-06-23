f469dd67142ce6e5895685c94a1e2c3cc29bb8c8
TERRAS Juba <ij_terras@esi.dz>
2022-06-23 17:46:26 +0100

    Modification ajout reservation

--- config/bd_test_integration.js
+++ config/bd_test_integration.js
@@ -8,6 +8,7 @@ const Pool = require("pg").Pool;
   port: 5432,
 });
 */
+process.env.DATABASE_URL="postgres://dovsksxwglnxdf:416dc753d9d1db383dec8e8ff7fdfdc14ffa7b1c8147a88860c1346cfd6adad1@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/d3vj2jdd55lhaf"
 const poolTestIntegration = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
